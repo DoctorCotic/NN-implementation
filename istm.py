51f83226fcd41cefc7056fb212ed09e87f873697
DoctorCotic <highfsociety@gmail.com>
2017-08-08 16:13:47 +0300

    edit istm

--- istm.py
+++ istm.py
@@ -5,8 +5,12 @@ from keras.layers import Dense, Activation, Embedding
 from keras.layers import LSTM, SpatialDropout1D
 from keras.datasets import imdb
 
+#Text analysis using recurrent neural networks
+
 np.random.seed(42)
+# Maximum number of words in the dictionary
 max_features = 5000
+# Length of a review in words
 maxlen = 80
 
 (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=max_features)
@@ -19,13 +23,13 @@ model = Sequential()
 model.add(Embedding(max_features, 32))
 model.add(SpatialDropout1D(0.2))
 model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+
+# layer for classification
 model.add(Dense(1, activation="sigmoid"))
+# Adam — adaptive moment estimation - optimization algorithm
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-
-model.fit(X_train, y_train, batch_size=64, epochs=7, validation_data=(X_test, y_test), verbose=2)
+model.fit(X_train, y_train, batch_size=64, epochs=9, validation_data=(X_test, y_test), verbose=2)
 scores = model.evaluate(X_test, y_test,
                         batch_size=64)
 print("The accuracy of the model on test data : %.2f%%" % (scores[1] * 100))

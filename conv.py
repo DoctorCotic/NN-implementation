4697cf9f3981f61abce04512e642dcbcd35fc1b4
DoctorCotic <highfsociety@gmail.com>
2017-08-08 14:38:54 +0300

    add comments

--- conv.py
+++ conv.py
@@ -9,6 +9,8 @@ from keras.optimizers import SGD
 
 numpy.random.seed(42)
 
+#Convolutional neural network for recognition of сifar10
+
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
 batch_size = 32
 nb_classes = 10
@@ -16,6 +18,7 @@ nb_epoch = 25
 img_rows, img_cols = 32, 32
 img_channels = 3
 
+#Normalize data on the intensity of image data
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 X_train /= 255
@@ -25,25 +28,32 @@ Y_train = np_utils.to_categorical(y_train, nb_classes)
 Y_test = np_utils.to_categorical(y_test, nb_classes)
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), padding='same',
-                        input_shape=(3, 32, 32), activation='relu'))
+#First convolution layer
+model.add(Conv2D(32, (3, 3), padding='same', input_shape=(3, 32, 32), activation='relu'))
+# Second convolution layer
 model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+# Sub -sample layer
 model.add(MaxPooling2D(pool_size=(2, 2)))
+# Regularization layer, that turns off neurons with a probability of 25%
 model.add(Dropout(0.25))
 
 model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
+
+# Transformation from multidimensional (N) to flat
 model.add(Flatten())
+# Fully connected layer
 model.add(Dense(512, activation='relu'))
 model.add(Dropout(0.5))
+# Output layer
 model.add(Dense(nb_classes, activation='softmax'))
 
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy',
-              optimizer=sgd,
-              metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy',optimizer=sgd, metrics=['accuracy'])
+
+# Training network
 model.fit(X_train, Y_train,
               batch_size=batch_size,
               epochs=nb_epoch,
--- mnist.py
+++ mnist.py
@@ -5,9 +5,14 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import np_utils
 
+
+# NN recognizes handwritten figures
+# The algorithm receives an image and needs to recognize the correct digit
+
 begin_time = time.time()
 numpy.random.seed(42)
 
+# There are 60 thousand images.  Fail with images and image tags
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 X_train = X_train.reshape(60000, 784)
@@ -18,24 +23,31 @@ X_test = X_test.astype('float32')
 X_train /= 255
 X_test /= 255
 
-# marks to category
+# marks to category (conversion of correct answers by Category)
+
 Y_train = np_utils.to_categorical(y_train, 10)
 Y_test = np_utils.to_categorical(y_test, 10)
 
 # create model
 model = Sequential()
 
-# add range of network
+# add range of network (enter - 800 neurons)
+# веса инициализируются случайно по нормальному распределению
 model.add(Dense(800, input_dim=784, activation="relu", kernel_initializer="normal"))
+# softmax- normalized exponential function
 model.add(Dense(10, activation="softmax", kernel_initializer="normal"))
 # add hidden layers
 model.add(Dense(800, input_dim=784, activation="relu", kernel_initializer="normal"))
 model.add(Dense(600, activation="relu", kernel_initializer="normal"))
 model.add(Dense(10, activation="softmax", kernel_initializer="normal"))
 
+#SGD - метод стохастического градиента
+
 model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
 
 print(model.summary())
+
+# batch_size- Size of the mini-selection;
 model.fit(X_train, Y_train, batch_size=25, epochs=125, validation_split=0.2, verbose=2)
 
 # evaluate the quality of the network training on the test data
